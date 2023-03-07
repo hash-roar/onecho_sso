bea850f446dacf10fbb7aee2af0a76cb9c3c0be2
hash-roar <1022647354@qq.com>
2023-03-07 15:55:27 +0800

    :tada:

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"onecho_sso_backend/models"
+	"onecho_sso_backend/pkg/gredis"
+	"onecho_sso_backend/pkg/logging"
+	"onecho_sso_backend/pkg/setting"
+	"onecho_sso_backend/routers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	setting.Setup()
+	logging.Setup()
+	models.Setup()
+	gredis.Setup()
+}
+
+func main() {
+	// gin.SetMode()
+	gin.SetMode(setting.ServerSetting.RunMode)
+	router := routers.InitRouter()
+	listen := fmt.Sprintf("%s", setting.ServerSetting.HttpPort)
+	logging.Info("start listen on: ", listen)
+
+	server := &http.Server{
+		Addr:           listen,
+		Handler:        router,
+		ReadTimeout:    setting.ServerSetting.ReadTimeout,
+		WriteTimeout:   setting.ServerSetting.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	server.ListenAndServe()
+}
---
+++ models/models.go
@@ -0,0 +1,30 @@
+package models
+
+import (
+	"fmt"
+	"log"
+	"onecho_sso_backend/pkg/setting"
+
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+func Setup() {
+	var err error
+	dsn := fmt.Sprintf("host=%s user=%s dbname=%s port=%s  password=%s sslmode=disable TimeZone=Asia/Shanghai",
+		setting.DatabaseSetting.Host,
+		setting.DatabaseSetting.User,
+		setting.DatabaseSetting.DbName,
+		setting.DatabaseSetting.Port,
+		setting.DatabaseSetting.Password)
+	fmt.Println(dsn)
+	db, err = gorm.Open(setting.DatabaseSetting.Type, dsn)
+	if err != nil {
+		log.Fatalf("models.Setup err: %v", err)
+	}
+	db.SingularTable(true)
+	db.DB().SetMaxIdleConns(10)
+	db.DB().SetMaxOpenConns(100)
+
+}
